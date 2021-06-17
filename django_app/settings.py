32f34aeffc84b7cf1d64164fcb2f8f0208ff16bd
ASO1801012 <1801012@s.asojuku.ac.jp>
2021-06-17 23:56:31 +0900

    new

--- django_app/settings.py
+++ django_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hon0!ry2fvh_sdi%xnmm77yrqg%_%pr0)3koxvm@d$_^4kcgmh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
